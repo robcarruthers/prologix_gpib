5565c5147850ffaf3e04cfba0da66b32a255124d
Rob Carruthers <robcarruthers@mac.com>
2019-10-10 12:25:21 +1300

    initial commit

---
+++ lib/prologix_gpib_usb.rb
@@ -0,0 +1,6 @@
+require "prologix_gpib_usb/version"
+
+module PrologixGpibUsb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/prologix_gpib_usb/version.rb
@@ -0,0 +1,3 @@
+module PrologixGpibUsb
+  VERSION = "0.1.0"
+end
